98b44c9ca3e94548c5d91474d928ab0e9af5cd15
Frank Liang <xiliang@redhat.com>
2026-05-20 16:26:32 +0800

    setup.py: set pkg long_description
    Signed-off-by: Frank Liang <teddy.x.liang@hotmail.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+current_directory = Path(__file__).parent
+long_description = (current_directory / "README.md").read_text()
 
 version = '0.2.1'
 
 setup(name='pytest-ssh',
       version=version,
       description="pytest plugin for ssh command run",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Xiao Liang',
       author_email='teddy.x.liang@hotmail.com',
       url='https://github.com/liangxiao1/pytest-ssh',
